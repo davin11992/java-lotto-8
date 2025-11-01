fca46d01b9b38fec7b42fea6086e13df8fbf7ddc
DavinLee <davin11992@gmail.com>
2025-11-02 06:44:10 +0900

    feat: 당첨 Rank 구현 및 테스트

---
+++ src/test/java/lotto/model/domain/RankTest.java
@@ -0,0 +1,38 @@
+package lotto.model.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class RankTest {
+
+    @Test
+    void 일등_테스트() {
+        assertThat(Rank.of(6, false)).isEqualTo(Rank.FIRST);
+    }
+
+    @Test
+    void 이등_테스트() {
+        assertThat(Rank.of(5, true)).isEqualTo(Rank.SECOND);
+    }
+
+    @Test
+    void 삼등_테스트() {
+        assertThat(Rank.of(5, false)).isEqualTo(Rank.THIRD);
+    }
+
+    @Test
+    void 사등_테스트() {
+        assertThat(Rank.of(4, false)).isEqualTo(Rank.FOURTH);
+    }
+
+    @Test
+    void 오등_테스트() {
+        assertThat(Rank.of(3, false)).isEqualTo(Rank.FIFTH);
+    }
+
+    @Test
+    void 낙첨_테스트() {
+        assertThat(Rank.of(2, false)).isEqualTo(Rank.LOSS);
+    }
+}
