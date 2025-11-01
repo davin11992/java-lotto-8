1be3ee5bdd84c9b667fa1ddedfb8dd8ba78490f1
DavinLee <davin11992@gmail.com>
2025-11-02 02:49:49 +0900

    feat: 당첨 번호 중복되는 경우 예외 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -14,6 +15,10 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개의 숫자여야 합니다.");
         }
+        if (numbers.size() != new HashSet<>(numbers).size()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 중복되지 않는 6개의 숫자여야 합니다.");
+        }
+        
     }
 
     // TODO: 추가 기능 구현
